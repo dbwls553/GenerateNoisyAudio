336928f6120b2aa4ce757fbe7bed5924e5450de0
유진 임 <dbwls553@naver.com>
2021-02-19 17:24:10 +0900

    완성

--- signal_processing.py
+++ signal_processing.py
@@ -1,28 +1,54 @@
-import get_signal_to_noise as stn
-from math import sqrt
+from math import sqrt, log10, ceil
 
 
 def calc_snr(signal, noise):
     """
-    Calculate signal noise ratio.
+    Calculate signal to noise ratio.
+    If file is not noisy then SNR is about 100dB.
     :param signal: (list)
     :param noise: (list)
     :return: (value) SNR(dB)
     """
+    if len(signal) != len(noise):
+        raise Exception("ERROR: Signal Noise size mismatch")
+    signal_power = calc_power(signal)
+    noise_power = calc_power(noise)
+    if noise_power == 0:
+        noise_power = pow(0.1, 10)
+    return 10 * log10(signal_power / noise_power)
 
-    pass
 
-
-def calc_ssnr(signal, noise, frame_size):
+def calc_ssnr(signal, noise, frame_size, select=None):
     """
     Calculate segmental signal noise ratio.
+    If file is not noisy then SNR is about 100dB.
     :param signal: (list)
     :param noise: (list)
     :param frame_size: (int) ssnr frame size
     :return: (value) SSNR(dB)
     """
-
-    pass
+    if len(signal) != len(noise):
+        raise Exception("ERROR: Signal Noise size mismatch")
+    number_of_frame_size = ceil(len(signal) / frame_size)
+    sum = 0
+    nonzero_frame_number = 0
+    segmental_signal_power = [0] * range(number_of_frame_size)
+    segmental_noise_power = [0] * range(number_of_frame_size)
+    segmental_signal_to_noise = [0] * range(number_of_frame_size)
+    for i in range(number_of_frame_size):
+        segmental_signal_power[i] = calc_power(signal[frame_size*i:frame_size*(i+1)])
+        segmental_noise_power[i] = calc_power(noise[frame_size*i:frame_size*(i+1)])
+        if segmental_noise_power[i] == 0:
+            segmental_noise_power[i] = pow(0.1, 10)
+        if segmental_signal_power[i] != 0:
+            nonzero_frame_number += 1
+            sum += 10 * log10(segmental_signal_power[i] / segmental_noise_power[i])
+            segmental_signal_to_noise[i] = segmental_signal_power[i] / segmental_noise_power[i]
+    ssnr = sum / nonzero_frame_number
+    if select == None:
+        return ssnr
+    else:
+        return ssnr, segmental_signal_to_noise
 
 def calc_power(input):
     """
@@ -60,11 +86,28 @@ def mix_noise(signal, noise, dB, snr_or_ssnr='snr', frame_size=None):
     :param dB: (value)
     :param snr_or_ssnr: 'snr' or 'ssnr'
     :param frame_size:  (int) only use in ssnr
-    :return: (list)
+    :return: (list) Noisy signal
     """
+    if len(signal) != len(noise):
+        raise Exception("ERROR: Signal Noise size mismatch")
+
     if snr_or_ssnr == 'snr':
-        pass
+        snr = calc_snr(signal, noise)
+        ratio = pow(10, (snr-dB)/20)
+        noisy_signal = noise.copy()
+        for i in range(len(noise)):
+            noisy_signal[i] = noise[i]*ratio + signal[i]
+        return noisy_signal
+
     elif snr_or_ssnr == 'ssnr':
-        pass
+        ssnr, segmental_signal_to_noise = calc_ssnr(signal, noise, frame_size, 1)
+        ratio = [1]*len(segmental_signal_to_noise)
+        noisy_signal = noise.copy()
+        for i in range(len(segmental_signal_to_noise)):
+            ratio[i] = pow(segmental_signal_to_noise[i], (ssnr-dB)/(2*ssnr))
+            for j in range(frame_size):
+                noisy_signal[frame_size*i + j] = noise[frame_size*i + j]*ratio[i] + signal[frame_size*i + j]
+        return noisy_signal
+
     else:
         raise Exception("ERROR: You must select only in range of snr or ssnr.")
